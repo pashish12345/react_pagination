8530765e825fed58df20965be2c2ae5787a92ae7
ashish <pashish12345@rediffmail.com>
2024-06-05 13:02:54 +0530

    to fetch initial data getData fun added

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Main = () => {
+  const [data, setData] = useState([]);
+  const [loading , setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+ async function getData(){
+
+try {
+  setLoading(true);
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  response = await response.json();
+  console.log(response);
+  setData(response)
+  
+} catch (error) {
+  setError(error)
+}finally{
+  setLoading(false)
+}
+
+  }
+
+
+  useEffect(()=>{
+   getData();
+  },[])
+  
   return (
     <div>
       

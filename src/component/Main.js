2efeff6612db9e9144bb41c11a730df5c4db9e16
ashish <pashish12345@rediffmail.com>
2024-06-07 15:01:33 +0530

    add  pahination functionality

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Data from './Data';
-
+import Paginate from './Paginate';
+const itemPerPage = 10;
 const Main = () => {
   const [data, setData] = useState([]);
   const [loading , setLoading] = useState(false);
   const [error, setError] = useState('');
-
+  const[currentPage, setCurrentPage] = useState(1);
+  
  async function getData(){
-
 try {
   setLoading(true);
   let response = await fetch('https://jsonplaceholder.typicode.com/posts');
   response = await response.json();
-  
   setData(response)
   
 } catch (error) {
@@ -23,6 +23,21 @@ try {
 
   }
 
+  let lastIndex = currentPage*itemPerPage
+  let firstIndex = currentPage*itemPerPage - itemPerPage
+  let dataForCurrentPage = data.slice(firstIndex,lastIndex);
+  let changePage=(pageNumber)=>{
+    setCurrentPage(pageNumber)
+  }
+
+  function previousPage(){
+if(currentPage > 0){
+  setCurrentPage(currentPage-1)
+}
+  }
+  function nextpage(){
+setCurrentPage(currentPage+1)
+  }
 
   useEffect(()=>{
    getData();
@@ -34,10 +49,18 @@ try {
         loading ? <h2> Loading ... </h2> : error ? <h2> {error?.name} </h2> : (
           <>
           {
-            data?.map(el=>{
+            dataForCurrentPage?.map(el=>{
              return <Data key={el.id} {...el} />
             })
           }
+          <Paginate 
+          itemPerPage={itemPerPage} 
+          currentPage={currentPage}
+          totalNoOfItems = {data?.length}
+          changePage={changePage}
+          previousPage={previousPage}
+          nextPage={nextpage}
+          />
           </>
         ) 
       }
--- src/component/Paginate.js
+++ src/component/Paginate.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const Paginate = () => {
+const Paginate = ({itemPerPage, currentPage,changePage,totalNoOfItems, previousPage,nextPage}) => {
+  
+
+let noOfPages = Math.ceil(totalNoOfItems/itemPerPage);
+console.log(noOfPages)
+let pageNumber =[];
+
+for(let i=1; i<= noOfPages ; i++){
+  pageNumber?.push(i)
+}
+
+
   return (
     <div>
       
+      <button type='button' href='#' disabled={currentPage <= 1} onClick={()=>previousPage()}>prev</button>
+      {
+        pageNumber?.map((el,i)=>{
+         return (
+         <>
+         
+          <span key={i}> 
+          
+           <a href='#' onClick={()=>changePage(el)}> {el} </a> </span>
+          
+         
+         </>
+         )
+        })
+      }
+       <button type='button' disabled={currentPage >= noOfPages} href='#'onClick={()=>nextPage()} >next</button>
     </div>
   )
 }
